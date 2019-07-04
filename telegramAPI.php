50f101cc4df98db1c10f5027adafe5b8edaa99d3
GreatHorizon <stas.gaisinn@gmail.com>
2019-07-04 20:27:55 +0300

    test

--- telegramAPI.php
+++ telegramAPI.php
@@ -5,7 +5,7 @@ use Telegram\Bot\Api;
 const apiToken = "680225339:AAFoHWnPG5KVG_9lD8IrbbBhqDmhYxtKyKE";
 $keyboard = [["Hello"], ["Show history"], ["Help"]];
 
-function getTelegramData(): object {
+function getTelegramData() {
     $telegram = new Api(apiToken); //Устанавливаем токен, полученный у BotFather
     return $telegram -> getWebhookUpdates(); 
 }
@@ -14,15 +14,15 @@ function getReplyMarkup($keyboard) {
     return getTelegramData()->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
 }
 
-function getText(object $result) {
+function getText($result) {
     return $result["message"]["text"]; //Текст сообщения
 }
 
-function getChatId(object $result) {
+function getChatId($result) {
     return $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 }
 
-function getUserName(object $result) {
+function getUserName($result) {
     return $result["message"]["from"]["username"]; //Юзернейм пользователя
 }
 
