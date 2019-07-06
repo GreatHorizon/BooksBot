6eb9d1c417fb7bfd4cc25ce5c71e9671155c05a8
GreatHorizon <stas.gaisinn@gmail.com>
2019-07-06 12:57:56 +0300

    test bd

--- index.php
+++ index.php
@@ -83,7 +83,7 @@
       $bookTitle = $bookInfo["items"][0]["volumeInfo"]["title"];
       $authors = $bookInfo["items"][0]["volumeInfo"]["authors"][0];
       $bookInfo = $bookInfo["items"][0]["volumeInfo"]["infoLink"];
-      addBookToHistory($bookTitle, $chatId);
+      addBookToHistory($bookInfo, $chatId);
       return "Name of the book: " . $bookTitle ."\nAuthor: ". $authors . " \nMore information about this book: " . $bookInfo . "";
     }
   }
