713a826789aee27891376832a030311823344de4
GreatHorizon <stas.gaisinn@gmail.com>
2019-07-07 12:57:01 +0300

    test

--- telegramAPI.php
+++ telegramAPI.php
@@ -1,8 +1,16 @@
 <?php
 
   use Telegram\Bot\Api;
-  $telegram = new Api(apiToken);
   const apiToken = "680225339:AAFoHWnPG5KVG_9lD8IrbbBhqDmhYxtKyKE";
+  $telegram = new Api(apiToken);
+  $keyboard = [["Hello"], ["My library"], ["Help"], ["Search"]];
+  $libraryKeyboard = [["Show library"], ["Add book"], ["Remove book"], ["Back"]];
+  $chatId = getChatId(getTelegramData($telegram));
+  $name = getUserName(getTelegramData($telegram));
+  $text = getText(getTelegramData($telegram));
+  $replyMarkup = getReplyMarkup($keyboard, $telegram);
+  $libraryKeyboardMarkUp = getReplyMarkup($libraryKeyboard, $telegram);
+  
  
   
   
